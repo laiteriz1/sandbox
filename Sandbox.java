cbc2353cc20929cdbb45bbf7409e6c61144a1172
Irmantas <laiteriz@gmail.com>
2022-04-20 19:50:00 +0300

    Very hello

--- Sandbox.java
+++ Sandbox.java
@@ -5,7 +5,7 @@ public class Sandbox {
 		
 	public static void main(String[] args) {			
 		try {		
-			System.out.println("Hello world");
+			System.out.println("Very hello world");
 			
 			
 		} catch(Exception e){			
