d0b63803d9d52ebdd3fd2e962a6f4c98bc725ff7
Irmantas <laiteriz@gmail.com>
2022-04-20 19:20:18 +0300

    Some more changes

---
+++ Sandbox.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Sandbox { 	
+		
+	public static void main(String[] args) {			
+		try {		
+			System.out.println("Hello world");
+			
+			
+		} catch(Exception e){			
+			e.printStackTrace();			
+			System.out.println("Unexpected error, sorry!");
+		} 			
+    }	
+}
